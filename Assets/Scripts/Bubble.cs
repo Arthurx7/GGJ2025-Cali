48e4d60d6e516545da154c772ebc302d7630e277
YinelaPrimero <yinela.primero@uao.edu.co>
2025-01-25 17:00:24 -0500

    jugabilidad base lista con algo de efectos

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -8,39 +8,67 @@ public class Bubble : MonoBehaviour
     private bool dificult;
     private int requiredClicks; 
     private int clicks = 0; 
-    private Image imageComponent;
 
-    [SerializeField] private LevelManager levelManager;
-    
+    private Image imageComponent;
+    private Button buttonComponent;
+
+    [SerializeField] private GameObject effects; 
+    [SerializeField] private GameObject effects2; 
+    // [SerializeField] private LevelManager levelManager;
+    [SerializeField] private Sprite bubbleExplote;
+
+    //Sonidos 
+     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip popSound; 
+    [SerializeField] private AudioClip explodeSound; 
+
 
     void Start()
     {
-        levelManager = FindObjectOfType<LevelManager>();
+        //levelManager = FindObjectOfType<LevelManager>();
        
         dificult = Random.value > 0.5f; 
         requiredClicks = dificult ? 2 : 1; 
 
         Debug.Log("Dificult: " + dificult + " Required Clicks: " + requiredClicks);
-
+       
         imageComponent = GetComponent<Image>();
+        buttonComponent = GetComponent<Button>();
+
+        audioSource = GetComponent<AudioSource>();
+        
+
     }
 
-    public void Clicked() {
-
+    public void Clicked()
+    {
         clicks++; 
+     
+        if (clicks == 1 && requiredClicks > 1)
+        {
+            PlaySound(popSound);
+            GameObject instantiatedEffect = Instantiate(effects2, transform.position, Quaternion.identity);
+        }
         
         Debug.Log("Clicked: " + clicks);
 
         if (clicks >= requiredClicks)
         {
-            imageComponent.enabled = false;
-            levelManager.ExplodeBubble();
-        }
+            PlaySound(explodeSound);
 
+            imageComponent.sprite = bubbleExplote;
+
+            buttonComponent.interactable = false; 
+            
+            //levelManager.ExplodeBubble(); 
+
+            GameObject instantiatedEffect = Instantiate(effects, transform.position, Quaternion.identity);
+        }
     }
-   
-    private void OnMouseDown()
+
+    private void PlaySound(AudioClip clip)
     {
-        
+    
+         audioSource.PlayOneShot(clip); 
     }
 }
